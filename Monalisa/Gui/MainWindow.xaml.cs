8ff6ff5f6969fb983f9367eadba457b7a0175dbb
David <d.festen@gmail.com>
2014-05-04 15:30:51 +0200

    made initial settings|picture|picture layout

--- Monalisa/Gui/MainWindow.xaml.cs
+++ Monalisa/Gui/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using org.monalisa.algorithm;
+﻿using Microsoft.Win32;
+using org.monalisa.algorithm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,10 +25,57 @@ namespace org.monalisa.gui
         public MainWindow()
         {
             InitializeComponent();
+            SetToDefaults();
 
             var painter = new Painter(MainCanvas);
             painter.Prepare(new TriangleTest());
             painter.Paint();
+        }
+
+        // Image uploader
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                SeedImage.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void TextBox_OnlyNumeric(object sender, TextCompositionEventArgs e)
+        {
+            if (!IsNaturalNumber(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Returns true iff is parsable to int.
+        /// </summary>
+        /// <param name="text">text to test.</param>
+        /// <returns>True if is a natural number, false otherwise.</returns>
+        private bool IsNaturalNumber(string text)
+        {
+            int result;
+            return int.TryParse(text, out result);
+        }
+
+        /// <summary>
+        /// Hardcoded defaults
+        /// TODO: Not hardcode the defaults ;)
+        /// </summary>
+        public void SetToDefaults()
+        {
+            TextBox_PopulationSize.Text = "50";
+            TextBox_CanvasSizeX.Text = "320";
+            TextBox_CanvasSizeY.Text = "240";
+            TextBox_PolygonCount.Text = "200";
+            ComboBox_PolygonType.SelectedIndex = 0;
         }
     }
 }
